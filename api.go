335db1857796464a968ddeb17ed4e274ff5fcdbe
Chris Sullivan <chrissullivan.dev@gmail.com>
2022-01-14 23:40:15 -0800

    Adding debugging line

--- api.go
+++ api.go
@@ -1,11 +1,12 @@
 package lambda
 
 import (
+	"fmt"
 	"os"
 )
 
 func Start(h interface{}) {
-
+	fmt.Println("IS_LAMBDA_AUTHORIZER", os.Getenv("IS_LAMBDA_AUTHORIZER"))
 	// Most common - probably
 	if os.Getenv("IS_LAMBDA_AUTHORIZER") != "true" {
 		ApiHandler(h.(ApiLambda))
